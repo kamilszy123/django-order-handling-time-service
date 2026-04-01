4f399c62b1b83ff6139acf55c2842731bc0691ad
kamil <kamil@cieszmysie.pl>
2026-04-01 14:09:18 +0200

    feat: add Account model with migrations

--- handling_time/models.py
+++ handling_time/models.py
@@ -1,3 +1,19 @@
+from django.utils import timezone
+from datetime import timedelta
+
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+
+    access_token = models.TextField(null=True, blank=True)
+    refresh_token = models.TextField(null=True, blank=True)
+    expires_at = models.DateTimeField(null=True, blank=True)
+
+    def is_token_expired(self):
+        if not self.expires_at:
+            return True
+        return self.expires_at < timezone.now() + timedelta(seconds=30)
+
+    def __str__(self):
+        return self.name
