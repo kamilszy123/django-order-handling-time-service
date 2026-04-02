614f46b1246e9f37a07e819fb7034666017df825
kamil <kamil@cieszmysie.pl>
2026-04-02 11:07:36 +0200

    feat: add AllegroExceptions

---
+++ integrations/allegro/exceptions.py
@@ -0,0 +1,12 @@
+
+class AllegroError(Exception):
+    def __init__(self, message, status_code=None):
+        self.status_code = status_code
+        super().__init__(message)
+
+
+class AllegroAuthError(AllegroError):
+    pass
+
+class AllegroAPIError(AllegroError):
+    pass
