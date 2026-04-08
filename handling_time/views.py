0d3f4a2d52a1fff8b2f60d0bd3b0c33ff458e74f
kamil <kamil@cieszmysie.pl>
2026-04-08 09:36:03 +0200

    feat: add AllegroGetOffersView endpoint

--- handling_time/views.py
+++ handling_time/views.py
@@ -2,8 +2,10 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from integrations.allegro.exceptions import AllegroError
+from integrations.allegro.service import get_offers, update_handling_time
 from .models import Account
-from integrations.allegro.auth import exchange_code_for_token, AllegroAuthError
+from integrations.allegro.auth import exchange_code_for_token
 
 
 class AllegroCallbackView(APIView):
@@ -11,12 +13,41 @@ class AllegroCallbackView(APIView):
         code = request.GET.get('code')
 
         if not code:
-            return Response({"error": "no code provided"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "no code provided"},
+                status=status.HTTP_400_BAD_REQUEST)
         try:
             account, _ = Account.objects.get_or_create(name="Allegro")
 
             exchange_code_for_token(account, code)
 
-            return Response({"status": "connected"}, status=status.HTTP_200_OK)
-        except AllegroAuthError as e:
-            return Response({"error": str(e)}, status=e.status_code or status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"status": "connected"},
+                status=status.HTTP_200_OK
+            )
+        except AllegroError as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code or status.HTTP_400_BAD_REQUEST)
+
+
+class AllegroGetOffersView(APIView):
+    def get(self, request):
+        try:
+            account = Account.objects.first()
+            if not account:
+                return Response(
+                    {"error": "No account configured"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            response = get_offers(account)
+            return Response(
+                {"offers": response},
+                status=status.HTTP_200_OK
+            )
+        except AllegroError as e:
+            return Response(
+                {"error getting offers": str(e)},
+                status=e.status_code or status.HTTP_502_BAD_GATEWAY
+            )
+
