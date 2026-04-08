1dfa012d3d624cfc38d6031cd0b1f040710b2761
kamil <kamil@cieszmysie.pl>
2026-04-08 10:20:16 +0200

    feat: add handling_time urls

--- handling_time/urls.py
+++ handling_time/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AllegroCallbackView
+from .views import AllegroCallbackView, AllegroGetOffersView, HandlingTimeConfigView
 
 urlpatterns = [
-    path('api/allegro/callback/', AllegroCallbackView.as_view())
+    path('api/allegro/callback/', AllegroCallbackView.as_view()),
+    path('offers/', AllegroGetOffersView.as_view()),
+    path('config/', HandlingTimeConfigView.as_view())
 ]
-
